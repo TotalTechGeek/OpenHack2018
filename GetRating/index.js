eaa519f4b49eb2331b18ff5b3b83dbc6b0b39da0
hacker1 <hacker1@OTAPRD503ops.onmicrosoft.com>
2018-10-24 16:40:53 +0000

    Updated index.js

--- GetRating/index.js
+++ GetRating/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('JavaScript HTTP trigger function processed a request.!!!!!!');
     // Ozi's name is being used here as a test. :)
 
     if (req.query.id || (req.body && req.body.id)) {
